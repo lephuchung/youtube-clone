9ba790b28b8e7a37b01b0efd462d6f89d072161d
lephuchung <lephuchung003@gmail.com>
2024-03-03 18:32:54 +0700

    update feed with api

--- src/data.js
+++ src/data.js
@@ -1,1 +1,11 @@
-export const API_KEY = 'AIzaSyC67uJ2fYZTleQpQg5U2K7qnV2ozYN6Y58'
+export const API_KEY = 'AIzaSyC67uJ2fYZTleQpQg5U2K7qnV2ozYN6Y58';
+
+export const value_converter = (value) => {
+    if (value >= 1000000) {
+        return Math.floor(value / 1000000) + 'M';
+    }
+    else if (value >= 1000) {
+        return Math.floor(value / 1000) + 'K';
+    }
+    else return value;
+}
