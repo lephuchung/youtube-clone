131e62d379481ea4d723791c115e96e3711cf51e
lephuchung <lephuchung003@gmail.com>
2024-03-04 01:13:58 +0700

    update playvideo info

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -8,8 +8,11 @@ import save from '../../assets/save.png';
 import user_profile from '../../assets/user_profile.jpg';
 import { API_KEY, value_converter } from "../../data";
 import moment from "moment";
+import { useParams } from "react-router-dom";
 
-const PlayVideo = ({ videoId }) => {
+const PlayVideo = () => {
+
+    const { videoId } = useParams();
 
     const [apiData, setApiData] = useState(null);
     const [channelData, setChannelData] = useState(null);
@@ -32,7 +35,7 @@ const PlayVideo = ({ videoId }) => {
 
     useEffect(() => {
         fetchVideoData();
-    }, [])
+    }, [videoId])
 
     useEffect(() => {
         fetchOtherData();
